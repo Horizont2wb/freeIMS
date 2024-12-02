8539551f180f5563aea89d3dd4bf4782b0f9959d
Willy Buecker <willy.buecker@yahoo.de>
2024-12-02 18:35:41 +0100

    added lager Model, Inventory Page, add Article page and layout

--- ims/ims/admin.py
+++ ims/ims/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Customer
+from .models import Lager
 
 # Register your models here.
 
-admin.site.register(Customer)
+admin.site.register(Lager)
--- ims/ims/models.py
+++ ims/ims/models.py
@@ -2,10 +2,14 @@ from django.db import models
 import datetime
 
 # Create your models here.
-class Customer(models.Model):
-    username = models.CharField(max_length=20)
-    email = models.CharField(max_length=50)
-    password = models.CharField(max_length=30)
+
+class Lager(models.Model):
+    id = models.AutoField(primary_key=True)
+    Barcode = models.CharField(max_length=30)
+    Bezeichnung = models.CharField(max_length=25)
+    Kategorie = models.CharField(max_length=20)
+    Lagerbestand = models.IntegerField(max_length=4)
+    Mindestbestand = models.IntegerField(max_length=4)
 
     def __str__(self):
-        return f'{self.username}'
+        return f'{self.id}'
